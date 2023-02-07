364b9582f47d8eb0336e53f6cfbff4b69d44be44
keduro12 <64760575+keduro12@users.noreply.github.com>
2023-02-07 13:09:22 -0500

    fd

--- src/views/InventarioView.vue
+++ src/views/InventarioView.vue
@@ -3,7 +3,7 @@
         <div class="card-body">
             <div class="table-responsive">
                 <table id="example" class="table table-striped table-bordered" style="width:100%">
-                    <thead >
+                    <thead>
                         <tr>
                             <th>Item</th>
                             <th>Id Item</th>
@@ -25,8 +25,24 @@
                             <td>{{ item.cantidad }}</td>
                             <td>{{ item.fechaIngreso }}</td>
                             
+                            <div class="card-body"> 
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" router.push="/additem"
+                                    viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round" class="feather feather-delete">
+                                    <path d="M21 4H8l-7 8 7 8h13a2 2 0 0 0 2-2V6a2 2 0 0 0-2-2z"></path>
+                                    <line x1="18" y1="9" x2="12" y2="15"></line>
+                                    <line x1="12" y1="9" x2="18" y2="15"></line>
+                                </svg>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                                    fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                                    stroke-linejoin="round" class="feather feather-edit">
+                                    <path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path>
+                                    <path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path>
+                                </svg>
+                            </div>
+
                         </tr>
-                        
+
                     </tbody>
                     <tfoot>
                         <div class="dataTables_paginate paging_simple_numbers" id="example_paginate">
@@ -60,12 +76,16 @@
 </template>
 
 <script setup>
-import {itemDatabase} from "@/store/itemDatabase.js"
+    import {
+        itemDatabase
+    } from "@/store/itemDatabase.js"
+    import { useRouter } from "vue-router";
 
 
-const useDataBase = itemDatabase();
+    const useDataBase = itemDatabase();
+    const router = useRouter();
 
-useDataBase.getItems()
+    useDataBase.getItems()
 </script>
 
 <style scoped>
